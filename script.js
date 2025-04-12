03370061d2655e1fdec3da79294ea1ff73fec59e
J-max <joelmtn7@gmail.com>
2025-04-12 10:06:34 +0100

    remain slide

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ AOS.init({
     offset: 100,
    
   });
+
+  const sidebar = document.getElementById('sidebar');
+  const menuIcon = document.getElementById('menu');
+  const cancelMenu = document.getElementById('cancelMenu');
+
+  const course1 = document.getElementById('course1');
+  const course2 = document.getElementById('course2');
+  const course3 = document.getElementById('course3');
+
+  const orderPop = document.getElementById('order');
+  const cancelOrderBtn = document.getElementById('cancelOrder');
+
+  function coursePop() {
+    orderPop.style.display = 'flex';
+
+  }
+
+  function cancelOrder() {
+    orderPop.style.display = 'none'
+  }
+
+  course1.addEventListener('click', coursePop);
+  course2.addEventListener('click', coursePop);
+  course3.addEventListener('click', coursePop);
+
+  cancelOrderBtn.addEventListener('click', cancelOrder)
+
+  menuIcon.addEventListener('click', showMenu)
+  cancelMenu.addEventListener('click', cancelDropdown)
+
+  function showMenu() {
+    sidebar.classList.add("show");
+  }
+
+  function cancelDropdown() {
+   sidebar.classList.remove("show");
+  }
+
+  
